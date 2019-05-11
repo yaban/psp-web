d1a18dcb7152006cdaaa70797b727949052dc4bf
Ferhat Yaban <ferhatyaban@gmail.com>
2019-05-11 23:08:59 +0300

    added http-proxy-middleware

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 const app = express();
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/psp-web'));
+
+const apiProxy = proxy('/api', { target: 'https://sandbox-reporting.rpdpymnt.com',  changeOrigin: true, secure: true  });
+app.use('/api', apiProxy);
+
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
